922ac37caadb847c6311366a9cad40b4a4d52f79
Geoffrey Wiederecht <gwiederecht11@berkeley.edu>
2018-02-15 18:04:29 -0800

    cmon

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,22 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    sum = 0
+    b = Array.new
+    i = 0
+    while i < a.length do
+      a[i] = a[i].to_i + 2
+      i += 1
+    end
+
+    a = a.uniq
+    for value in a do
+      if value.even? && value < 10 then
+        sum += value
+      end
+    end
+
+    return sum
+
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,38 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  sum = a + b
+  return sum * sum
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  n = a.length
+  i = 0
+  while i < n do
+    minIndex = i
+    k = i + 1
+    while k < n do
+      if a[k] < a[minIndex] then
+        minIndex = k
+      end
+      k +=1
+    end
+    temp = a[minIndex]
+    a[minIndex] = a[i]
+    a[i] = temp + 1
+    i+=1
+
+  end
+
+  return a
+
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+
+  return first_name + " " + last_name
+
 
 end
 
@@ -48,5 +71,14 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
+  sum = 0
+  for p in 0...word.length do
+    a = word[p].chr.to_sym
+    sum+= values[a]
+
+  end
+
+  return sum
+
   # Q5 CODE HERE
 end
